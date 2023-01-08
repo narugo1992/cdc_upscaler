f0f6732b61eba3096df5a7d859023a2c3bd5a0f5
narugo1992 <narugo@126.com>
2023-01-08 13:29:15 +0800

    dev(narugo): update unittest

--- test/test_upscale.py
+++ test/test_upscale.py
@@ -8,12 +8,10 @@ class TestCdcUpscale:
     def test_image_upscale(self):
         image_upscale(
             'images/2797632045_90956483_p0_master1200_1_y.jpg',
-            'output/result.png'
         )
 
     def test_image_upscale_psize_256(self):
         image_upscale(
             'images/2797632045_90956483_p0_master1200_1_y.jpg',
-            'output/result.png',
             psize=256,
         )
