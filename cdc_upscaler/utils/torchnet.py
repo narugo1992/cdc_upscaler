79faf7ce5870c2cfb5eabcc54f43e5576d3d6684
narugo1992 <narugo@126.com>
2023-01-09 00:36:03 +0800

    dev(narugo): refactor rm module part

--- cdc_upscaler/utils/torchnet.py
+++ cdc_upscaler/utils/torchnet.py
@@ -1,5 +1,4 @@
 import logging
-from collections import OrderedDict
 from functools import lru_cache
 
 import torch
@@ -29,19 +28,24 @@ def load_weights(model, weights='', gpus=1, strict=True, resume=False, just_weig
     try:
         model.load_state_dict(model_weights, strict=strict)
     except:
-        logging.info('Loading from DataParallel module ...')
-        model = _rm_module(model, model_weights)
+        model.load_state_dict(_weights_rm_module(model_weights), strict=strict)
     logging.info(f'Loading {weights} success!')
 
     if gpus > 1:
+        logging.info(f'Loading from DataParallel module, due to gpus={gpus!r} ...')
         model = nn.DataParallel(model, device_ids=[i for i in range(gpus)])
     return model
 
 
-def _rm_module(model, weights):
-    new_state_dict = OrderedDict()
-    for k, v in weights.items():
-        name = k[7:]  # remove `module.`
-        new_state_dict[name] = v
-    model.load_state_dict(new_state_dict)
-    return model
+def _weights_rm_module(weights):
+    w = {}
+    for key, value in weights.items():
+        segments = key.split('.')
+        assert segments[0] == 'module'
+
+        # remove `module.` prefix,
+        # `module.HG_5.skip_conv0.res.0.weight` --> `HG_5.skip_conv0.res.0.weight`
+        key = '.'.join(segments[1:])
+        w[key] = value
+
+    return w
