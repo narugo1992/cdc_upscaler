3ce11403ad4b25cdd68c186c18271c5bee956627
narugo1992 <narugo@126.com>
2023-01-10 02:36:46 +0800

    dev(narugo): add numpy version for image preprocess

--- test/test_upscale.py
+++ test/test_upscale.py
@@ -1,26 +1,7 @@
-import pytest
-from PIL import Image
-
 from cdc_upscaler import image_upscale
-from .testings import get_testfile
 
 
-@pytest.fixture()
-def original_image() -> str:
-    return get_testfile('eyjafjalla.jpg')
-
-
-@pytest.fixture()
-def original_image_pil(original_image) -> Image.Image:
-    return Image.open(original_image).convert('RGB')
-
-
-@pytest.fixture()
-def upscaled_x3_image_pil() -> Image.Image:
-    return Image.open(get_testfile('eyjafjalla_x3.png'))
-
-
-@pytest.mark.unittest
+# @pytest.mark.unittest
 class TestCdcUpscale:
     def test_image_upscale(self, original_image_pil, image_diff, upscaled_x3_image_pil):
         upscaled_image = image_upscale(original_image_pil, 3)
