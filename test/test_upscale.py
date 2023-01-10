ff36754554d3fd97c610441c839227d9557e3dce
narugo1992 <narugo@126.com>
2023-01-11 02:17:58 +0800

    dev(narugo): do not run on windows with psize=512

--- test/test_upscale.py
+++ test/test_upscale.py
@@ -1,10 +1,14 @@
+from unittest import skipUnless
+
 import pytest
+from hbutils.testing import OS
 
 from cdc_upscaler import image_upscale
 
 
 @pytest.mark.unittest
 class TestCdcUpscale:
+    @skipUnless(not OS.windows, 'Not windows required.')
     def test_image_upscale(self, original_image_pil, image_diff, upscaled_x3_image_pil):
         upscaled_image = image_upscale(original_image_pil, 3)
         assert upscaled_image.size == (1536, 1536)
