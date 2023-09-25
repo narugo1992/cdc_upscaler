93239b04adfbf6515d8cbf7dde70041f59cbb32e
narugo1992 <narugo@126.com>
2023-09-25 21:58:08 +0800

    dev(narugo): release as version v0.2.0

--- cdc_upscaler/config/meta.py
+++ cdc_upscaler/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = "cdc_upscaler"
 
 #: Version of this project.
-__VERSION__ = "0.1.1"
+__VERSION__ = "0.2.0"
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'Image upscaler for anime.'
